d4bab22318b49d2fa1773958d1b05d727b25081d
RyanDhaliwal <150496383+RyanDhaliwal@users.noreply.github.com>
2025-07-30 16:45:45 -0600

    Delete ListKeeper directory

--- ListKeeper/ListKeeper.Web/src/app/models/search-criteria.ts
+++
@@ -1,5 +0,0 @@
-export interface SearchCriteria {
-  searchText?: string;
-  showOnlyCompleted?: boolean;
-  statuses: number[]; // Array of status values: 0=All, 1=Upcoming, 2=Past Due, 3=Completed
-}
